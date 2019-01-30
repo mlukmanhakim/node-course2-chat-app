427d3040ccc93cb7c65e5981377feb51901759b3
M Lukman Hakim <lukman821@gmail.com>
2019-01-30 14:13:58 +0800

    Heroku

--- server/server.js
+++ server/server.js
@@ -6,5 +6,5 @@ var app = express();
 const port = 8000;
 app.use(express.static(publicPath));
 app.listen(port, ()=>{
-    console.log("Server run on port",port);
+    console.log(`Server run on port ${port}`);
 }); 
