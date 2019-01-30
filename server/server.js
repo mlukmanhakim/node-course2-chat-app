e3fdddc796559870a124186d141dcc96fae1e1b2
M Lukman Hakim <lukman821@gmail.com>
2019-01-30 13:48:44 +0800

    init commit

---
+++ server/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+
+var app = express();
+const port = 8000;
+app.use(express.static(publicPath));
+app.listen(port, ()=>{
+    console.log("Server run on port",port);
+}); 
